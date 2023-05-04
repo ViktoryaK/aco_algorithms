bf3373ac14321159993dcfdd35617dd50296a12a
KOlegaBB <yaroslav.klym@ucu.edu.ua>
2023-05-04 12:15:54 +0300

    finished constant analysing

--- src/scripts/ant_colony_tsp_script.py
+++ src/scripts/ant_colony_tsp_script.py
@@ -3,12 +3,12 @@ import subprocess
 from math import inf
 import matplotlib.pyplot as plt
 import numpy as np
-
+from statistics import stdev
 
 def time_graph(times, func_number):
     threads = [1, 2, 4, 6, 8, 12]
     print(times)
-    coefficient_list = [times[0]/time/threads[i] for i, time in enumerate(times)]
+    coefficient_list = [times[0] / time / threads[i] for i, time in enumerate(times)]
     print(coefficient_list)
 
     bar_width = 0.25
@@ -22,7 +22,6 @@ def time_graph(times, func_number):
     plt.savefig(f"data/efficiency{func_number}.jpg")
     plt.cla()
 
-    bar_width = 0.25
     br1 = np.arange(len(threads))
     plt.bar(br1, times, color='darkviolet', width=bar_width)
     plt.xlabel("Number of threads")
@@ -31,6 +30,17 @@ def time_graph(times, func_number):
     plt.title(f"Time plot for function {func_number}")
     plt.legend()
     plt.savefig(f"data/time{func_number}.jpg")
+    plt.cla()
+
+    coefficient_list = [times[0]/time for _, time in enumerate(times)]
+    br1 = np.arange(len(threads))
+    plt.plot(br1, coefficient_list, color='darkviolet')
+    plt.xlabel("Number of threads")
+    plt.ylabel("Amdal`s curve")
+    plt.xticks([r for r in range(len(threads))], threads)
+    plt.title(f"Amdal`s law curve for function {func_number}")
+    plt.legend()
+    plt.savefig(f"data/amdal{func_number}.jpg")
     plt.cla()
 
 
@@ -47,13 +57,15 @@ def run_task_par(graph_path, config_path, paths_path, func_num, thread):
 def get_res_par(graph_path, config_path, config_path_mm, paths_path, n, threads):
     funcs_res = []
     total_times = []
-    for thread in threads:
-        print(f"Running thread {thread}")
+    for i in range(1, 4):
         current_times = []
-        for i in range(1, 4):
-            print(f"Running func: {i}")
-            funcs_res.append([])
-            for _ in range(n):
+        print(f"Running func: {i}")
+        funcs_res.append([])
+        for thread in threads:
+            thread_times = []
+            print(f"Running thread {thread}")
+            for k in range(n):
+                print(f"Running: {k}")
                 if i == 3:
                     config_path = config_path_mm
                 func_res = run_task_par(graph_path, config_path, paths_path, i, thread)
@@ -61,9 +73,11 @@ def get_res_par(graph_path, config_path, config_path_mm, paths_path, n, threads)
                 for j in range(0, len(func_res) - 1, 2):
                     res_n.append((float(func_res[j].split(": ")[1]), float(func_res[j + 1].split(": ")[1])))
                 funcs_res[-1].append(res_n)
-                current_times.append(func_res[-1].split("=")[1])
+                thread_times.append(int(func_res[-1].split("=")[1]))
+            current_times.append(thread_times)
         total_times.append(current_times)
     return funcs_res, total_times
+
 
 def run_task(graph_path, config_path, paths_path, func_num):
     task_paths = "./bin/aco_algorithms"
@@ -74,14 +88,13 @@ def run_task(graph_path, config_path, paths_path, func_num):
     output = output_str.split("\n")
     return output[:-1]
 
-
 def get_res(graph_path, config_path, config_path_mm, paths_path, n):
     funcs_res = []
     total_times = []
     for i in range(1, 5):
         print(f"Running func: {i}")
         funcs_res.append([])
-        total_times.append([])
+        cur_times = []
         for _ in range(n):
             if i == 4:
                 config_path = config_path_mm
@@ -90,8 +103,10 @@ def get_res(graph_path, config_path, config_path_mm, paths_path, n):
             for j in range(0, len(func_res) - 1, 2):
                 res_n.append((float(func_res[j].split(": ")[1]), float(func_res[j + 1].split(": ")[1])))
             funcs_res[-1].append(res_n)
-            total_times[-1].append(func_res[-1].split("=")[1])
+            cur_times.append(int(func_res[-1].split("=")[1]))
+        total_times.append(cur_times)
     return funcs_res, total_times
+
 
 def build_graph_min(min_res, min_value, number_of_f, path_to_save_min):
     titles = ["ACO", "Elitism", "ACS", "MMAS"]
@@ -112,13 +127,21 @@ def build_graph_min(min_res, min_value, number_of_f, path_to_save_min):
     plt.cla()
 
 
-def build_graph_avg(avg_res, min_value, number_of_f, path_to_save_avg):
+def build_graph_avg(avg_res, min_value, number_of_f, path_to_save_avg, stdev_for_avg):
+    print(path_to_save_avg)
     titles = ["ACO", "Elitism", "ACS", "MMAS"]
+    most_popular = []
+    minimum_value = []
+    for elem in stdev_for_avg[number_of_f]:
+        most_popular.append(stdev([el[0] for el in elem]))
+        minimum_value.append(stdev([el[1] for el in elem]))
     x = [elem[0] for elem in avg_res[number_of_f]]
     y = [i for i, j in enumerate(avg_res[number_of_f])]
-    plt.plot(y, x, label='Most popular')
+    plt.errorbar(y, x, yerr=most_popular, ecolor='blue', label='Most popular')
+    plt.scatter(y, x, s=10)
     x_1 = [elem[1] for elem in avg_res[number_of_f]]
-    plt.plot(y, x_1, label='Minimum')
+    plt.errorbar(y, x_1, yerr=minimum_value, ecolor='red', label='Minimum')
+    plt.scatter(y, x_1, s=10)
     plt.axhline(y=min_value)
     plt.xlabel('Number of iterations')
     plt.ylabel('Path length')
@@ -148,7 +171,6 @@ def build_graph(res_list):
     plt.scatter(x, y[0], marker='o')
     plt.savefig("output.png")
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Python script for comparison of different functions')
@@ -157,22 +179,35 @@ if __name__ == "__main__":
     parser.add_argument('dir_path', type=str, help='Path to directory')
     parser.add_argument('config', type=str, help='Path to config file')
     parser.add_argument('config_min_max', type=str, help='Path to min max config file')
-    parser.add_argument('is_parallel', type=bool, help='True or false')
+    parser.add_argument('is_parallel', type=int, help='True or false')
     args = parser.parse_args()
     dir_path = args.dir_path
     config_temp = dir_path + "/config_temp.cfg"
     paths_temp = dir_path + "/path_temp.csv"
     min_path = dir_path + "/min_path.txt"
-    threads = [1, 2, 4, 6, 8, 12]
+    threads = [2, 4, 6, 8, 12]
     if args.is_parallel:
-        results, times = get_res_par(args.graph, args.config, args.config_min_max, paths_temp, args.repetitions, threads)
-        time_graph([int(time[0]) for time in times], '1')
-        time_graph([int(time[1]) for time in times], '2')
-        time_graph([int(time[2]) for time in times], '3')
+        results, times = get_res_par(args.graph, args.config, args.config_min_max, paths_temp, args.repetitions,
+                                     threads)
+        results_seq, times_seq = get_res(args.graph, args.config, args.config_min_max, paths_temp, args.repetitions)
+        min_list = [[min(times_seq[0])], [min(times_seq[1])], [min(times_seq[2])]]
+        print(times)
+        for i in range(3):
+            for j in range(len(threads)):
+                min_list[i].append(min(times[i][j]))
+        time_graph(min_list[0], '1')
+        time_graph(min_list[1], '2')
+        time_graph(min_list[2], '3')
     else:
         results, times = get_res(args.graph, args.config, args.config_min_max, paths_temp, args.repetitions)
         min_res = []
         avg_res = []
+        dev_for = []
+        for res_i in results:
+            dev_for_i = []
+            for i in range(len(res_i[0])):
+                dev_for_i.append([r[i] for r in res_i])
+            dev_for.append(dev_for_i)
         for res_i in results:
             minimum = inf
             min_res_i = res_i[0]
@@ -191,18 +226,17 @@ if __name__ == "__main__":
                 avg_res_i[edge][1] /= len(res_i)
             min_res.append(min_res_i)
             avg_res.append(avg_res_i)
-
         with open(min_path, "r") as min_v:
             for line in min_v:
                 minimum_value = float(line.strip())
         build_graph_min(min_res, minimum_value, 0, f"{dir_path}/min_0.jpg")
-        build_graph_avg(avg_res, minimum_value, 0, f"{dir_path}/avg_0.jpg")
+        build_graph_avg(avg_res, minimum_value, 0, f"{dir_path}/avg_0.jpg", dev_for)
         build_graph_min(min_res, minimum_value, 1, f"{dir_path}/min_1.jpg")
-        build_graph_avg(avg_res, minimum_value, 1, f"{dir_path}/avg_1.jpg")
+        build_graph_avg(avg_res, minimum_value, 1, f"{dir_path}/avg_1.jpg", dev_for)
         build_graph_min(min_res, minimum_value, 2, f"{dir_path}/min_2.jpg")
-        build_graph_avg(avg_res, minimum_value, 2, f"{dir_path}/avg_2.jpg")
+        build_graph_avg(avg_res, minimum_value, 2, f"{dir_path}/avg_2.jpg", dev_for)
         build_graph_min(min_res, minimum_value, 3, f"{dir_path}/min_3.jpg")
-        build_graph_avg(avg_res, minimum_value, 3, f"{dir_path}/avg_3.jpg")
+        build_graph_avg(avg_res, minimum_value, 3, f"{dir_path}/avg_3.jpg", dev_for)
     # print(len(results))
     # print(len(results[0]))
     # print(len(results[0][0]))
--- src/scripts/constant_analysing.py
+++ src/scripts/constant_analysing.py
@@ -1,5 +1,21 @@
 import argparse
-from ant_colony_tsp_script import get_res
+from ant_colony_tsp_script import get_res, run_task
+from statistics import stdev, mean
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def get_func_res(graph_path, config_path, paths_path, n, func):
+    funcs_res = []
+    total_times = []
+    for _ in range(n):
+        func_res = run_task(graph_path, config_path, paths_path, func)
+        res_n = []
+        for j in range(0, len(func_res) - 1, 2):
+            res_n.append((float(func_res[j].split(": ")[1]), float(func_res[j + 1].split(": ")[1])))
+        funcs_res.append(res_n)
+        total_times.append(int(func_res[-1].split("=")[1]))
+    return funcs_res, total_times
 
 
 def build_ant_dep_table(dir, graph_path, config_path, config_path_mm, paths_path, n):
@@ -9,7 +25,7 @@ def build_ant_dep_table(dir, graph_path, config_path, config_path_mm, paths_path
     final_mins = [[], [], [], []]
     for ant in ant_numbers:
         print(f"Cur ant: {ant}")
-        deposition_mins = [[],[],[],[]]
+        deposition_mins = [[], [], [], []]
         for deposition in depositions:
             print(f"Cur deposition: {deposition}")
             config_txt = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = {deposition}\n' \
@@ -18,8 +34,8 @@ def build_ant_dep_table(dir, graph_path, config_path, config_path_mm, paths_path
             with open(config_path, 'w') as file:
                 file.write(config_txt)
             config_txt_mm = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = {deposition}\n' \
-                             f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = {ant}\nexplore_const = 0.001\n' \
-                             f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
+                            f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = {ant}\nexplore_const = 0.001\n' \
+                            f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
                             f'min_pheromone = 0.0001\nmax_pheromone = 0.01'
             with open(config_path_mm, 'w') as file:
                 file.write(config_txt_mm)
@@ -43,7 +59,264 @@ def build_ant_dep_table(dir, graph_path, config_path, config_path_mm, paths_path
     return final_mins
 
 
-def ant_n_graph(dir, graph_path, config_path, config_path_mm, paths_path, n):
+def build_imp_table(dir, graph_path, config_path, config_path_mm, paths_path, n):
+    methods_dirs = ["default", "elitism", "acs", "min_max"]
+    pheromones = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    heuristics = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    final_mins = [[], [], [], []]
+    for pheromone in pheromones:
+        print(f"Pheromones: {pheromone}")
+        heuristic_mins = [[], [], [], []]
+        for heuristic in heuristics:
+            print(f"Cur heuristic: {heuristic}")
+            config_txt = f'nodes = 50\npheromone_importance = {pheromone}\nheuristic_inf = {heuristic}\ndeposition = 10\n' \
+                         f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = 0.001\n' \
+                         f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0'
+            with open(config_path, 'w') as file:
+                file.write(config_txt)
+            config_txt_mm = f'nodes = 50\npheromone_importance = {pheromone}\nheuristic_inf = {heuristic}\n' \
+                            f'deposition = 10\n' \
+                            f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = 0.001\n' \
+                            f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
+                            f'min_pheromone = 0.0001\nmax_pheromone = 0.01'
+            with open(config_path_mm, 'w') as file:
+                file.write(config_txt_mm)
+            res, times = get_res(graph_path, config_path, config_path_mm, paths_path, n)
+            for i in range(4):
+                avg = 0
+                for elem in res[i]:
+                    avg += elem[-1][1]
+                heuristic_mins[i].append(avg / n)
+        for i in range(4):
+            final_mins[i].append(heuristic_mins[i])
+    q = 0
+    for el in final_mins:
+        print(methods_dirs[q])
+        for i in range(len(el)):
+            text = ""
+            for j in range(len(el[i])):
+                text += str(el[i][j]) + " "
+            print(text)
+        q += 1
+    return final_mins
+
+
+def build_init_graph(dir, graph_path, config_path, config_path_mm, paths_path, n, output_path):
+    methods_dirs = ["default", "elitism", "acs", "min_max"]
+    pheromones = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+    final_mins = [[], [], [], []]
+    min_value = 5104.525367305101
+    for pheromone in pheromones:
+        print(f"Pheromones: {pheromone}")
+        config_txt = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                     f'evaporation_rate = 0.9\ninit_pheromone = {pheromone}\nants_n = 30\nexplore_const = 0.001\n' \
+                     f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0'
+        with open(config_path, 'w') as file:
+            file.write(config_txt)
+        config_txt_mm = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                        f'evaporation_rate = 0.9\ninit_pheromone = {pheromone}\nants_n = 30\nexplore_const = 0.001\n' \
+                        f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
+                        f'min_pheromone = 0.0001\nmax_pheromone = 0.01'
+        with open(config_path_mm, 'w') as file:
+            file.write(config_txt_mm)
+        res, times = get_res(graph_path, config_path, config_path_mm, paths_path, n)
+
+        for i in range(4):
+            last_min_res = []
+            for elem in res[i]:
+                last_min_res.append(elem[-1][1])
+            final_mins[i].append(last_min_res)
+    print(final_mins)
+    titles = ["ACO", "Elitism", "ACS", "MMAS"]
+    stdev_list = [[], [], [], []]
+    avg_list = [[], [], [], []]
+    for i, func_mins in enumerate(final_mins):
+        for elem in func_mins:
+            stdev_list[i].append(stdev(elem))
+            avg_list[i].append(mean(elem))
+        x = pheromones
+        y = avg_list[i]
+        bar_width = 0.25
+        br1 = np.arange(len(x))
+        plt.bar(br1, y, yerr=stdev_list[i], ecolor='red', color='darkviolet', width=bar_width)
+        plt.xticks([r for r in range(len(x))], x)
+        plt.axhline(y=min_value)
+        plt.xlabel('Init pheromone')
+        plt.ylabel('Path length')
+        title = "Init pheromone dependency for " + titles[i] + " algorithm"
+        plt.ylim(min_value, 6000)
+        plt.yticks(np.arange(min_value // 100 * 100, 6000, 100))
+        plt.title(title)
+        plt.legend()
+        plt.savefig(output_path + f"/init_pheromone_cl_{i}")
+        plt.cla()
+
+
+def build_evaropation_graph(dir, graph_path, config_path, config_path_mm, paths_path, n, output_path):
+    methods_dirs = ["default", "elitism", "acs", "min_max"]
+    evaropations = [x / 10 for x in range(10)]
+    final_mins = [[], [], [], []]
+    min_value = 5104.525367305101
+    for evaropation in evaropations:
+        print(f"Evaropation: {evaropation}")
+        config_txt = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                     f'evaporation_rate = {evaropation}\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = 0.001\n' \
+                     f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0'
+        with open(config_path, 'w') as file:
+            file.write(config_txt)
+        config_txt_mm = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                        f'evaporation_rate = {evaropation}\ninit_pheromone = 0.000001\nants_n = 30\n' \
+                        f'explore_const = 0.001\n' \
+                        f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
+                        f'min_pheromone = 0.0001\nmax_pheromone = 0.01'
+        with open(config_path_mm, 'w') as file:
+            file.write(config_txt_mm)
+        res, times = get_res(graph_path, config_path, config_path_mm, paths_path, n)
+
+        for i in range(4):
+            last_min_res = []
+            for elem in res[i]:
+                last_min_res.append(elem[-1][1])
+            final_mins[i].append(last_min_res)
+    print(final_mins)
+    titles = ["ACO", "Elitism", "ACS", "MMAS"]
+    stdev_list = [[], [], [], []]
+    avg_list = [[], [], [], []]
+    for i, func_mins in enumerate(final_mins):
+        for elem in func_mins:
+            stdev_list[i].append(stdev(elem))
+            avg_list[i].append(mean(elem))
+        x = evaropations
+        y = avg_list[i]
+        bar_width = 0.25
+        br1 = np.arange(len(x))
+        plt.bar(br1, y, yerr=stdev_list[i], ecolor='red', color='darkviolet', width=bar_width)
+        plt.xticks([r for r in range(len(x))], x)
+        plt.axhline(y=min_value)
+        plt.xlabel('Evaropation rate')
+        plt.ylabel('Path length')
+        title = "Evaropation rate dependency for " + titles[i] + " algorithm"
+        plt.ylim(min_value, 6000)
+        plt.yticks(np.arange(min_value // 100 * 100, 6000, 100))
+        plt.title(title)
+        plt.legend()
+        plt.savefig(output_path + f"/evaropation_rate_cl_{i}")
+        plt.cla()
+
+
+def build_explore_graph(dir, graph_path, config_path, paths_path, n, output_path):
+    explorations = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 0.5, 0.9]
+    final_mins = []
+    min_value = 5104.525367305101
+    for exploration in explorations:
+        print(f"Exploration: {exploration}")
+        config_txt = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                     f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = {exploration}\n' \
+                     f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0'
+        with open(config_path, 'w') as file:
+            file.write(config_txt)
+        res, times = get_func_res(graph_path, config_path, paths_path, n, 3)
+        last_min_res = []
+        for elem in res:
+            last_min_res.append(elem[-1][1])
+        final_mins.append(last_min_res)
+    print(final_mins)
+    stdev_list = []
+    avg_list = []
+    for elem in final_mins:
+        stdev_list.append(stdev(elem))
+        avg_list.append(mean(elem))
+    x = explorations
+    y = avg_list
+    bar_width = 0.25
+    br1 = np.arange(len(x))
+    plt.bar(br1, y, yerr=stdev_list, ecolor='red', color='darkviolet', width=bar_width)
+    plt.xticks([r for r in range(len(x))], x)
+    plt.axhline(y=min_value)
+    plt.xlabel('Exploration constant')
+    plt.ylabel('Path length')
+    title = "Exploration constant for ACS algorithm"
+    plt.ylim(min_value, 6000)
+    plt.yticks(np.arange(min_value // 100 * 100, 6000, 100))
+    plt.title(title)
+    plt.legend()
+    plt.savefig(output_path + f"/exploration_const_cl")
+    plt.cla()
+
+
+def build_decay_graph(dir, graph_path, config_path, paths_path, n, output_path):
+    decays = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1]
+    final_mins = []
+    min_value = 5104.525367305101
+    for decay in decays:
+        print(f"Decay: {decay}")
+        config_txt = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\ndeposition = 10\n' \
+                     f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = 0.001\n' \
+                     f'pheromone_decay = {decay}\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0'
+        with open(config_path, 'w') as file:
+            file.write(config_txt)
+        res, times = get_func_res(graph_path, config_path, paths_path, n, 3)
+        last_min_res = []
+        for elem in res:
+            last_min_res.append(elem[-1][1])
+        final_mins.append(last_min_res)
+    print(final_mins)
+    stdev_list = []
+    avg_list = []
+    for elem in final_mins:
+        stdev_list.append(stdev(elem))
+        avg_list.append(mean(elem))
+    x = decays
+    y = avg_list
+    bar_width = 0.25
+    br1 = np.arange(len(x))
+    plt.bar(br1, y, yerr=stdev_list, ecolor='red', color='darkviolet', width=bar_width)
+    plt.xticks([r for r in range(len(x))], x)
+    plt.axhline(y=min_value)
+    plt.xlabel('Pheromone decay')
+    plt.ylabel('Path length')
+    title = "Pheromone decay for ACS algorithm"
+    plt.ylim(min_value, 6000)
+    plt.yticks(np.arange(min_value // 100 * 100, 6000, 100))
+    plt.title(title)
+    plt.legend()
+    plt.savefig(output_path + f"/pheromone_decay_cl")
+    plt.cla()
+
+
+def build_min_max_table(dir, graph_path, config_path_mm, paths_path, n, output_path):
+    methods_dirs = ["default", "elitism", "acs", "min_max"]
+    min_ph = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+    max_ph = [0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10, 100]
+    final_mins = [[], [], [], []]
+    for ph_1 in min_ph:
+        print(f"Min: {ph_1}")
+        max_mins = []
+        for ph_2 in max_ph:
+            if (ph_2 <= ph_1):
+                max_mins.append(0)
+                continue
+            print(f"Max: {ph_2}")
+            config_txt_mm = f'nodes = 50\npheromone_importance = 1\nheuristic_inf = 5\n' \
+                            f'deposition = 10\n' \
+                            f'evaporation_rate = 0.9\ninit_pheromone = 0.000001\nants_n = 30\nexplore_const = 0.001\n' \
+                            f'pheromone_decay = 0.001\nelitism_n = 5\nmax_iter = 30\nmutation_rate = 0.1\n' \
+                            f'min_pheromone = {ph_1}\nmax_pheromone = {ph_2}'
+            with open(config_path_mm, 'w') as file:
+                file.write(config_txt_mm)
+            res, times = get_func_res(graph_path, config_path_mm, paths_path, n, 4)
+            avg = 0
+            for elem in res:
+                avg += elem[-1][1]
+            max_mins.append(avg / n)
+        final_mins.append(max_mins)
+    for i in range(len(final_mins)):
+        text = ""
+        for j in range(len(final_mins[i])):
+            text += str(final_mins[i][j]) + " "
+        print(text)
+    return final_mins
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -54,5 +327,5 @@ if __name__ == "__main__":
     parser.add_argument('temp_config', type=str, help='Path to temporary configuration file')
     parser.add_argument('temp_config_mm', type=str, help='Path to temporary configuration file')
     args = parser.parse_args()
-    build_ant_dep_table(args.dir_path, args.graph, args.temp_config, args.temp_config_mm, "./data/graph3/path_tems.csv",
-                        args.repetitions)
+    build_min_max_table(args.dir_path, args.graph, args.temp_config_mm, "./data/graph3/path_tems.csv",
+                      args.repetitions, "./data/graph3/analysing/")
